d22dbe643bee1a4161c7679b8828f5cf78952a91
Aldo Diaz <aldo@theqontrol.com>
2026-08-04 15:03:06 -0600

    cambios de idea

---
+++ landing/studio-qontrol-blog/sanity.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'sanity';
+import { structureTool } from 'sanity/structure';
+import { schemaTypes } from './schemaTypes';
+export default defineConfig({
+    name: 'default',
+    title: 'Qontrol Blog',
+    projectId: 'km94b5r9',
+    dataset: 'production',
+    plugins: [structureTool()],
+    schema: {
+        types: schemaTypes,
+    },
+});
